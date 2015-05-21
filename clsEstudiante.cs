5c750bbcaae31d875d0b1a87e287a5aa71db4a84
jaop2316 <jessikita.ldu@gmail.com>
2015-05-21 14:28:06 -0500

    Primera Version ClsEstudiante

---
+++ clsEstudiante.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Estudiantes_Proyecto.Entidad
+{
+    public class clsEstudiante
+    {
+        public clsEstudiante()
+        {
+        }
+
+        private int _idEstudiante;
+
+        public int IdEstudiante
+        {
+            get { return _idEstudiante; }
+            set { _idEstudiante = value; }
+        }
+        private string _nombre;
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+        private string _apellido;
+
+        public string Apellido
+        {
+            get { return _apellido; }
+            set { _apellido = value; }
+        }
+        private string _cedula;
+
+        public string Cedula
+        {
+            get { return _cedula; }
+            set { _cedula = value; }
+        }
+
+
+    }
+}
