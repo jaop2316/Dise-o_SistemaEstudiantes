b219921c644796ee01e85af0d3592f1eb59528d2
jaop2316 <jessikita.ldu@gmail.com>
2015-05-23 18:40:38 -0500

    Cambios de ClsEstudiante para ajustar a la BD_Estudiantes

--- clsEstudiante.cs
+++ clsEstudiante.cs
@@ -3,53 +3,78 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Estudiantes_Proyecto.Entidad
+/// <summary>
+/// Summary description for clsEstudiante
+/// </summary>
+public class clsEstudiante
 {
-    public class clsEstudiante
+    public clsEstudiante(string idEstudiante, string nombre, string apellido, string cedula, double nota1, double nota2, double nota3)
     {
-        public clsEstudiante()
-        {
-        }
-//Atributos
-        private int _idEstudiante;
-        private string _nombre;
-        private string _apellido;
-        private string _cedula;
-        private double _nota;
-
-
-//Metodos set y get
-
-        public int IdEstudiante
-        {
-            get { return _idEstudiante; }
-            set { _idEstudiante = value; }
-        }
-
-        public string Nombre
-        {
-            get { return _nombre; }
-            set { _nombre = value; }
-        }
- 
-        public string Apellido
-        {
-            get { return _apellido; }
-            set { _apellido = value; }
-        }
-
-        public string Cedula
-        {
-            get { return _cedula; }
-            set { _cedula = value; }
-        }
-
-        public double Nota
-        {
-            get { return _nota; }
-            set { _nota = value; }
-        }
-
-
+        this.IdEstudiante = idEstudiante;
+        this.Nombre = nombre;
+        this.Apellido = apellido;
+        this.Cedula = cedula;
+        this.Nota = nota1;
+        this.Nota2 = nota2;
+        this.Nota3 = nota3;
+        this.NotaTotal = 0;
     }
+    //Atributos
+    private string _idEstudiante;
+    private string _nombre;
+    private string _apellido;
+    private string _cedula;
+    private double _nota;
+    private double _nota2;
+    private double _nota3;
+    private double _notaTotal;
+
+
+    //Metodos set y get
+
+    public string IdEstudiante
+    {
+        get { return _idEstudiante; }
+        set { _idEstudiante = value; }
+    }
+    public double Nota2
+    {
+        get { return _nota2; }
+        set { _nota2 = value; }
+    }
+    public double Nota3
+    {
+        get { return _nota3; }
+        set { _nota3 = value; }
+    }
+    public string Nombre
+    {
+        get { return _nombre; }
+        set { _nombre = value; }
+    }
+
+    public string Apellido
+    {
+        get { return _apellido; }
+        set { _apellido = value; }
+    }
+
+    public string Cedula
+    {
+        get { return _cedula; }
+        set { _cedula = value; }
+    }
+
+    public double Nota
+    {
+        get { return _nota; }
+        set { _nota = value; }
+    }
+    public double NotaTotal
+    {
+        get { return _notaTotal; }
+        set { _notaTotal = value; }
+    }
+
+
 }
