01bda6aa6def1d155a7fe9004b56b23c7b4d5555
jaop2316 <jessikita.ldu@gmail.com>
2015-05-28 01:52:26 -0500

    Creacion de Triggers para el registro de Notas

--- Script_Base_de_datos_Creacion.sql
+++ Script_Base_de_datos_Creacion.sql
@@ -1,4 +1,5 @@
 
+/*Ejecutar las setencias en el orden en el que estan*/
 create database ESTUDIANTES
 go
 
@@ -16,13 +17,134 @@ create table Notas(
 codigoEstudiante char(3) PRIMARY KEY,
 notaUno dec(3,1) check(notaUno > 0 and notaUno <=10),
 notaDos dec(3,1) check(notaDos > 0 and notaDos <=10),
-notaTres dec(3,1) default (null) check(notaTres > 0 and notaTres <=10),
-notaFinal dec(4,1) default (null) check(notaFinal > 0 and notaFinal <=10),
+notaTres dec(3,1) default (null) check(notaTres > 0 and notaTres <=20),
+notaFinal dec(4,1) default (null) check(notaFinal > 0 and notaFinal <=40),
 statusAprueba char(1) default (null) check(statusAprueba in('A','F','S')),
 FOREIGN KEY(codigoEstudiante) REFERENCES Estudiantes (codigoEstudiante)
 )
 
-select *from Estudiantes
+create table aprobados(codigoEstudiante char(3) foreign key references Estudiantes(codigoEstudiante), 
+nombreEstudiante varchar(50),apellidoEstudiante varchar(50),
+primary Key(codigoEstudiante))
+
+create table supletorio(codigoEstudiante char(3) foreign key references Estudiantes(codigoEstudiante), 
+nombreEstudiante varchar(50),apellidoEstudiante varchar(50),
+primary Key(codigoEstudiante))
+
+create table reprobados(codigoEstudiante char(3) foreign key references Estudiantes(codigoEstudiante), 
+nombreEstudiante varchar(50),apellidoEstudiante varchar(50),
+primary Key(codigoEstudiante))
+
+/*Trigger para controlar la insercion de las notas , que sume las notas y de la final*/
+create trigger tr_InserccionNotas 
+on Notas 
+after insert 
+as 
+declare @nota1 dec(3,1) ,
+@nota2 dec(3,1),
+@notafinal dec(3,1)
+
+set @nota1=(select i.notaUno from inserted i ,Notas n where n.codigoEstudiante=i.codigoEstudiante)
+set @nota2=(select i.notaDos from inserted i, Notas n where n.codigoEstudiante=i.codigoEstudiante)
+set @notafinal=@nota1+@nota2
+
+update Notas set notaFinal=@nota1+@nota2 from Notas n , inserted i 
+where n.codigoEstudiante=i.codigoEstudiante
+if(@notafinal>=14)
+update Notas set statusAprueba='A' from Notas n, inserted i
+where n.codigoEstudiante=i.codigoEstudiante
+else
+begin
+if(@notafinal>=9 and @notafinal<14)
+update Notas set statusAprueba='S'from Notas n, inserted i
+where n.codigoEstudiante=i.codigoEstudiante
+else
+begin 
+update Notas set statusAprueba='F'from Notas n, inserted i
+where n.codigoEstudiante=i.codigoEstudiante
+end
+end
+
+/*TRigger para dar el estatus atraves del cual se pone al estudiante en su tabla si ha 
+aprobado , fallado o suple*/
+create trigger tr_InsercionAprobadosSuplePierde
+on Notas 
+after insert 
+as 
+declare
+@nota1 dec(3,1) ,
+@nota2 dec(3,1),
+@notafinal dec(3,1),
+@nombreEstudiante varchar(50),
+@apellidoEstudiante varchar(50),
+@codigoEstudiante char(3)
+set @nota1=(select i.notaUno from inserted i ,Notas n where n.codigoEstudiante=i.codigoEstudiante)
+set @nota2=(select i.notaDos from inserted i, Notas n where n.codigoEstudiante=i.codigoEstudiante)
+set @notafinal=@nota1+@nota2
+set @apellidoEstudiante=(select apellidoEstudiante from inserted i, Notas n inner join 
+Estudiantes e on e.codigoEstudiante=n.codigoEstudiante where i.codigoEstudiante=e.codigoEstudiante)
+set @nombreEstudiante=(select nombreEstudiante from inserted i, Notas n inner join 
+Estudiantes e on e.codigoEstudiante=n.codigoEstudiante where i.codigoEstudiante=e.codigoEstudiante)
+set @codigoEstudiante= (select i.codigoEstudiante from inserted i )
+if(@notafinal>=14)
+insert into aprobados values (@codigoEstudiante,@nombreEstudiante,@apellidoEstudiante)
+else
+begin
+if(@notafinal>=9 and @notafinal<14)
+insert into supletorio values(@codigoEstudiante,@nombreEstudiante,@apellidoEstudiante)
+else 
+begin 
+insert into reprobados values(@codigoEstudiante,@nombreEstudiante,@apellidoEstudiante)
+end
+end
+
+/*Trigger para que mediante un update se ingrese la nota del supletorio y se sume a la nota para 
+ver si pasa o no*/
+alter trigger tr_Supletorio 
+on Notas
+after update 
+as 
+declare 
+@statusAprueba char(1),
+@NotaFinal decimal(4,1)
+set @statusAprueba=(select n.statusAprueba from Notas n , inserted i  
+where n.codigoEstudiante=i.codigoEstudiante)
+set @NotaFinal=(select n.notaFinal from Notas n , inserted i
+where n.codigoEstudiante=i.codigoEstudiante)
+if(@statusAprueba='S')
+begin
+if update(notaTres)
+begin 
+update Notas set notaFinal=i.notaTres+@NotaFinal from Notas n , inserted i 
+where n.codigoEstudiante=i.codigoEstudiante
+end
+end
+/*Trigger actualiza el estado despues del ingresar la tecera nota del suple*/
+create trigger tr_ApruebaFinal
+on Notas
+after update 
+as 
+declare 
+@statusAprueba char(1),
+@NotaFinal decimal(4,1),
+@NotaTres decimal(4,1)
+set @statusAprueba=(select n.statusAprueba from Notas n , inserted i  
+where n.codigoEstudiante=i.codigoEstudiante)
+set @NotaFinal=(select n.notaFinal from Notas n , inserted i
+where n.codigoEstudiante=i.codigoEstudiante)
+set @notaTres=(select i.notaTres from inserted i)
+if(@statusAprueba='S')
+begin
+if update(notaTres)
+begin 
+if (@NotaFinal+@NotaTres>=24)
+update Notas set statusAprueba='A' from Notas n , inserted i 
+where n.codigoEstudiante=i.codigoEstudiante
+end
+end
+
+select *from Notas
+select *from supletorio
 
 insert into Estudiantes values ('E01','Arciniega','Jessica')
 insert into Estudiantes values ('E02','Betancourt','Patricio')
@@ -57,4 +179,19 @@ insert into Notas values ('E12',8.0,6.0,default,default,default)
 insert into Notas values ('E13',10.0,5.0,default,default,default)
 insert into Notas values ('E14',9.0,8.0,default,default,default)
 insert into Notas values ('E15',8.0,7.5,default,default,default)
+
+/*update para probar notas de supletorio*/
+update Notas set notaTres=15 where codigoEstudiante='E03'
+
+
+
+
+select *from Notas
+select *from aprobados
+select *from reprobados 
+select *from supletorio
+select *from Estudiantes
+
+
+
 
